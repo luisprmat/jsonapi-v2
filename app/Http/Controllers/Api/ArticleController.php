06ce164402ec12841e4e43c0a663f1f83ac56a2a
Luis Antonio Parrado <luisprmat@gmail.com>
2021-08-04 21:13:01 -0500

    Implements route to fetch all articles, resource object and collections

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Article;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ArticleResource;
+use App\Http\Resources\ArticleCollection;
 
 class ArticleController extends Controller
 {
+    public function index(): ArticleCollection
+    {
+        return ArticleCollection::make(Article::all());
+    }
+
     public function show(Article $article): ArticleResource
     {
         return ArticleResource::make($article);
