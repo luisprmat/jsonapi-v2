320461f42510fc8ea6c3a562ae17ac55ee2c585a
Luis Antonio Parrado <luisprmat@gmail.com>
2021-08-04 20:44:54 -0500

    Implements route for getting a single article

---
+++ app/Http/Controllers/Api/ArticleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ArticleResource;
+
+class ArticleController extends Controller
+{
+    public function show(Article $article): ArticleResource
+    {
+        return ArticleResource::make($article);
+    }
+}
