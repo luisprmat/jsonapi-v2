94e365df38c362a0895452a430f7dc1e40b46077
Luis Antonio Parrado <luisprmat@gmail.com>
2021-10-27 12:59:53 -0500

    Filter resources with JSON:API spec

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -7,15 +7,29 @@ use App\Http\Requests\SaveArticleRequest;
 use App\Http\Resources\ArticleCollection;
 use App\Http\Resources\ArticleResource;
 use App\Models\Article;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Str;
 
 class ArticleController extends Controller
 {
     public function index(): ArticleCollection
     {
-        $articles = Article::allowedSorts(['title', 'content']);
+        $articles = Article::query();
+
+        // filters
+        $allowedFilters = ['title', 'content', 'year', 'month'];
+
+        foreach (request('filter', []) as $filter => $value) {
+            abort_unless(in_array($filter, $allowedFilters), 400);
+            if ($filter === 'year') {
+                $articles->whereYear('created_at', $value);
+            } else if ($filter === 'month') {
+                $articles->whereMonth('created_at', $value);
+            } else {
+                $articles->where($filter, 'LIKE', '%' . $value . '%');
+            }
+        }
+
+        $articles->allowedSorts(['title', 'content']);
 
         return ArticleCollection::make($articles->jsonPaginate());
     }
---
+++ tests/Feature/Articles/FilterArticlesTest.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Tests\Feature\Articles;
+
+use App\Models\Article;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FilterArticlesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_filter_articles_by_title()
+    {
+        Article::factory()->create([
+            'title' => 'Aprende Laravel Desde Cero'
+        ]);
+
+        Article::factory()->create([
+            'title' => 'Other Article'
+        ]);
+
+        // articles?filter[title]=Laravel
+        $url = route('api.v1.articles.index', [
+            'filter' => [
+                'title' => 'Laravel'
+            ]
+        ]);
+
+        $this->getJson($url)
+            ->assertJsonCount(1, 'data')
+            ->assertSee('Aprende Laravel Desde Cero')
+            ->assertDontSee('Other Article')
+        ;
+    }
+
+    /** @test */
+    public function can_filter_articles_by_content()
+    {
+        Article::factory()->create([
+            'content' => 'Aprende Laravel Desde Cero'
+        ]);
+
+        Article::factory()->create([
+            'content' => 'Other Article'
+        ]);
+
+        // articles?filter[content]=Laravel
+        $url = route('api.v1.articles.index', [
+            'filter' => [
+                'content' => 'Laravel'
+            ]
+        ]);
+
+        $this->getJson($url)
+            ->assertJsonCount(1, 'data')
+            ->assertSee('Aprende Laravel Desde Cero')
+            ->assertDontSee('Other Article')
+        ;
+    }
+
+    /** @test */
+    public function can_filter_articles_by_year()
+    {
+        Article::factory()->create([
+            'title' => 'Article from 2021',
+            'created_at' => now()->year(2021)
+        ]);
+
+        Article::factory()->create([
+            'title' => 'Article from 2022',
+            'created_at' => now()->year(2022)
+        ]);
+
+        // articles?filter[year]=2021
+        $url = route('api.v1.articles.index', [
+            'filter' => [
+                'year' => '2021'
+            ]
+        ]);
+
+        $this->getJson($url)
+            ->assertJsonCount(1, 'data')
+            ->assertSee('Article from 2021')
+            ->assertDontSee('Article from 2022')
+        ;
+    }
+
+    /** @test */
+    public function can_filter_articles_by_month()
+    {
+        Article::factory()->create([
+            'title' => 'Article from March',
+            'created_at' => now()->month(3)
+        ]);
+
+        Article::factory()->create([
+            'title' => 'Another article from March',
+            'created_at' => now()->month(3)
+        ]);
+
+        Article::factory()->create([
+            'title' => 'Article from January',
+            'created_at' => now()->month(1)
+        ]);
+
+        // articles?filter[month]=3
+        $url = route('api.v1.articles.index', [
+            'filter' => [
+                'month' => '3'
+            ]
+        ]);
+
+        $this->getJson($url)
+            ->assertJsonCount(2, 'data')
+            ->assertSee('Article from March')
+            ->assertSee('Another article from March')
+            ->assertDontSee('Article from January')
+        ;
+    }
+
+    /** @test */
+    public function cannot_filter_articles_by_unknown_filters()
+    {
+        Article::factory()->count(2)->create();
+
+        // articles?filter[unknown]=filter
+        $url = route('api.v1.articles.index', [
+            'filter' => [
+                'unknown' => 'filter'
+            ]
+        ]);
+
+        $this->getJson($url)->assertStatus(400);
+    }
+}
