db8050590105c8d0154c24706cbcb1ea31608e96
Luis Antonio Parrado <luisprmat@gmail.com>
2021-10-27 19:34:44 -0500

    Use of Route::bind

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,11 @@
 use App\Http\Controllers\Api\ArticleController;
 use Illuminate\Support\Facades\Route;
 
+Route::bind('article', function ($article) {
+    return App\Models\Article::where('slug', $article)
+        ->sparseFieldset()
+        ->firstOrFail();
+});
+
 Route::apiResource('articles', ArticleController::class)
     ->names('api.v1.articles');
