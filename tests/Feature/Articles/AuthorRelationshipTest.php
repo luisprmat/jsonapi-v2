5e57e08ac6cff96b6a8b4eb4419e91ffbc7caa6f
Luis Antonio Parrado <luisprmat@gmail.com>
2022-02-20 00:48:58 -0500

    Validation of relationship links

--- tests/Feature/Articles/AuthorRelationshipTest.php
+++ tests/Feature/Articles/AuthorRelationshipTest.php
@@ -80,4 +80,26 @@ class AuthorRelationshipTest extends TestCase
             'user_id' => $author->id
         ]);
     }
+
+    /** @test  */
+    function author_must_exist_in_database()
+    {
+        $article = Article::factory()->create();
+
+        $url = route('api.v1.articles.relationships.author', $article);
+
+        $this->withoutJsonApiDocumentFormatting();
+
+        $this->patchJson($url, [
+            'data' => [
+                'type' => 'authors',
+                'id' => 'non-existing'
+            ]
+        ])->assertJsonApiValidationErrors('data.id');
+
+        $this->assertDatabaseHas('articles', [
+            'title' => $article->title,
+            'user_id' => $article->user_id
+        ]);
+    }
 }
--- tests/Feature/Articles/CategoryRelationshipTest.php
+++ tests/Feature/Articles/CategoryRelationshipTest.php
@@ -79,4 +79,26 @@ class CategoryRelationshipTest extends TestCase
             'category_id' => $category->id
         ]);
     }
+
+    /** @test  */
+    function category_must_exist_in_database()
+    {
+        $article = Article::factory()->create();
+
+        $url = route('api.v1.articles.relationships.category', $article);
+
+        $this->withoutJsonApiDocumentFormatting();
+
+        $this->patchJson($url, [
+            'data' => [
+                'type' => 'categories',
+                'id' => 'non-existing'
+            ]
+        ])->assertJsonApiValidationErrors('data.id');
+
+        $this->assertDatabaseHas('articles', [
+            'title' => $article->title,
+            'category_id' => $article->category_id
+        ]);
+    }
 }
