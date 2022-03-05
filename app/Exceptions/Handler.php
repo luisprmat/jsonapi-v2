323766907a90ef5c58889fd7aa2b8b26d71662b3
Luis Antonio Parrado <luisprmat@gmail.com>
2022-03-05 15:42:46 -0500

    Intercept http requests in Laravel

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,10 +2,10 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Validation\ValidationException;
 use App\Http\Responses\JsonApiValidationErrorResponse;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Validation\ValidationException;
-use Throwable;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -36,8 +36,17 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            $id = $request->input('data.id');
+            $type = $request->input('data.type');
+
+            return response()->json([
+                'errors' => [
+                    'title' => 'Not Found',
+                    'detail' => "No records found with the id '{$id}' in the '{$type}' resource.",
+                    'status' => '404'
+                ]
+            ], 404);
         });
     }
 
